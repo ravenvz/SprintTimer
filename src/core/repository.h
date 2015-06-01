0f511cd5bfa732f35df2c76f9859791445bba035
Pavel <texort@gmail.com>
2015-06-01 16:38:36 +0300

    Add repository stub

---
+++ src/core/repository.h
@@ -0,0 +1,14 @@
+#ifndef REPOSITORY_H
+#define REPOSITORY_H
+
+class PomodoroRepository
+{
+public:
+    PomodoroRepository ();
+    virtual ~PomodoroRepository ();
+
+private:
+    /* data */
+};
+
+#endif // REPOSITORY_H
