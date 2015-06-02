f15a09580b59a4faa4b8c3abfe7fbb18267e7d78
Pavel <texort@gmail.com>
2015-06-02 17:19:27 +0000

    Implement dirty variant of repository pattern

---
+++ src/data_base/db_helper.c
@@ -0,0 +1,4 @@
+#include "db_helper.h"
+
+
+
--- src/data_base/db_helper.h
+++ src/data_base/db_helper.h
@@ -1,6 +1,14 @@
-#include <QtSql>
+#ifndef DB_HELPER_H
+#define DB_HELPER_H
 
-void createDatabase(QSqlDatabase& db, QString& filename) {
+
+#include <QtSql>
+#include <QSqlDatabase>
+#include "core/entities.h"
+
+
+// void createDatabase(QSqlDatabase& db, QString& filename);
+inline void createDatabase(QSqlDatabase& db, QString& filename) {
     db.setDatabaseName(filename);
     db.open();
     QSqlQuery query;
@@ -9,22 +17,88 @@ void createDatabase(QSqlDatabase& db, QString& filename) {
             "name varchar(50), "
             "start_time datetime, "
             "finish_time datetime)");
+    db.close();
 }
 
-
-bool connectToDatabase() {
+// bool createDbConnection();
+inline bool createDbConnection() {
     QString filename = "db/pomodoro.db";
     QSqlDatabase db = QSqlDatabase::addDatabase("QSQLITE");
-    QFile database(filename);
-    if (!database.exists()) {
+    QFile databaseFile(filename);
+    if (!databaseFile.exists()) {
         qDebug() << "Database not found. Creating...";
         createDatabase(db, filename);
     } else {
         qDebug() << "Database found. Opening...";
         db.setDatabaseName(filename);
-        db.open();
     }
-    return db.isOpen();
-    db.close();
-    QSqlDatabase::removeDatabase("QSQLITE");
+    if (!db.open()) {
+        return false;
+    }
+    return true;
 }
+
+
+// std::shared_ptr<QSqlDatabase> getDbConnection() {
+//     QString filename = "db/pomodoro.db";
+//     // QSqlDatabase db = QSqlDatabase::addDatabase("QSQLITE");
+//     std::shared_ptr<QSqlDatabase> db_ptr(new QSqlDatabase(QSqlDatabase::addDatabase("QSQLITE")));
+//     (*db_ptr).setDatabaseName(filename);
+//     return db_ptr;
+// }
+//
+
+class IDataMapper
+{
+public:
+    IDataMapper ();
+    virtual ~IDataMapper ();
+
+private:
+    /* data */
+};
+
+
+class PomodoroMapper
+{
+public:
+    // PomodoroMapper ();
+    // virtual ~PomodoroMapper ();
+
+    // bool create(Pomodoro pomodoro);
+        
+
+private:
+    /* data */
+};
+
+class PomodoroRepository
+{
+public:
+    PomodoroRepository() :
+        dataMapper(PomodoroMapper())
+    {}
+    // virtual ~PomodoroRepository ();
+    QStringList getPomodorosForToday() {
+        QStringList result;
+        QSqlQuery query;
+        query.exec("select * from pomodoro");
+        while (query.next()) {
+            QStringList m;
+            m.append(query.value(0).toString());
+            m.append(query.value(1).toString());
+            m.append(query.value(2).toString());
+            m.append(query.value(3).toString());
+            QString r = m.join(" ");
+            result.append(r);
+        }
+        return result;
+    }
+
+    // bool storePomodoro(Pomodoro pomodoro);
+
+private:
+    PomodoroMapper dataMapper;
+};
+
+#endif // DB_HELPER_H
