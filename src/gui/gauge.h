9afa914a5524de46de052d94b66607b0d5915103
Pavel <texort@gmail.com>
2015-09-27 21:05:33 +0300

    Enchance Gauge widget
    * Number of completed pomodoros is now displayed on mouse hover
    * Size of pie is now calculated correctly
    * Pie border is now a bit thickier, that improves looks

--- src/gui/gauge.h
+++ src/gui/gauge.h
@@ -2,22 +2,28 @@
 #define GAUGE_H
 
 #include <QWidget>
+#include <QEvent>
+
 
 class Gauge : public QWidget
 {
     Q_OBJECT
+
 public:
     explicit Gauge(unsigned actual, unsigned goal, QWidget* parent = 0);
+
 protected:
-    void paintEvent(QPaintEvent*);
+    void paintEvent(QPaintEvent*) override;
+    bool eventFilter(QObject* object, QEvent* event) override;
+
 private:
     unsigned actual = 0;
     unsigned goal = 0;
+    bool displayDetails = false;
     const QColor normalWork = QColor("#6baa15");
     const QColor overwork = Qt::red;
     const QColor noWork = Qt::gray;
-    const QRectF size = QRectF(10, 10, 30, 30);
-    const QRectF gaugeSize = QRectF(9.5, 9.5, 29.5, 29.5);
+    const QRectF size = QRectF(3, 3, 30, 30);
     const int offsetToTop = 90 * 16;
     const int fullCircle = 360 * 16;
 };
