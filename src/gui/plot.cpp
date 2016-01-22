2138d123d377275b4260cbc1647bd85107f4e2a0
Pavel <texort@gmail.com>
2016-01-22 20:14:23 +0300

    Refactor Plot implementation

--- src/gui/plot.cpp
+++ src/gui/plot.cpp
@@ -6,43 +6,13 @@
 #include <QDebug>
 
 
-Graph::Graph() {
-
+void AxisRange::setRange(double start, double end) {
+    this->start = std::min(start, end);
+    this->end = std::max(start, end);
 }
 
-void Graph::setData(GraphData& data) {
-    points = data;
-    std::sort(points.begin(), points.end(), [](const auto& point1, const auto& point2) {
-            return point1.x < point2.x;
-        });
-}
-
-void Graph::clearData() {
-    points.clear();
-}
-
-void Graph::setPen(QPen& pen) {
-    this->pen = pen;
-}
-
-const QPen Graph::getPen() const {
-    return pen;
-}
-
-const GraphPoint& Graph::operator[](std::size_t idx) const {
-    return points[idx];
-}
-
-size_t Graph::size() const {
-    return points.size();
-}
-
-void Graph::setShowPoints(bool showPoints) {
-    mShowPoints = showPoints;
-}
-
-bool Graph::showPoints() const {
-    return mShowPoints;
+double AxisRange::span() const {
+    return end - start;
 }
 
 
@@ -66,10 +36,76 @@ void Plot::setGraphData(int graphNum, GraphData& data) {
     adaptiveSizeComputed = false;
 }
 
+void Plot::reset() {
+    for (auto& points : pointBoxes) {
+        points.clear();
+    }
+    for (auto& graph : graphs) {
+        graph.clearData();
+    }
+}
+
+void Plot::replot() {
+    repaint();
+}
+
+void Plot::setRangeX(double start, double end) {
+    rangeX.setRange(start, end);
+}
+
+void Plot::setRangeY(double start, double end) {
+    rangeY.setRange(start, end);
+}
+
+void Plot::paintEvent(QPaintEvent*) {
+    QPainter painter(this);
+    painter.setRenderHint(QPainter::Antialiasing);
+    if (!adaptiveSizeComputed)
+        computeAdaptiveSizes();
+    if (pointBoxes.empty()) {
+        painter.eraseRect(availableRect);
+        return;
+    }
+
+    for (int graphNum = 0; graphNum < graphs.size(); ++graphNum) {
+        paintGraph(graphNum, painter);
+    }
+}
+
+void Plot::mouseMoveEvent(QMouseEvent* event) {
+    const auto& toolTip = getPosTooltip(event->pos());
+    if (toolTip.size() != 0) {
+        const QPoint toolTipPos {event->globalPos() - QPoint{int(pointBoxSize),
+                                 toolTipOffset * int(pointBoxSize)}};
+        QToolTip::showText(toolTipPos, toolTip);
+    } else {
+        QToolTip::hideText();
+        event->ignore();
+    }
+}
+
+void Plot::onSizeComputed() {
+    constructPointBoxes();
+    adaptiveSizeComputed = true;
+}
+
+void Plot::computeAdaptiveSizes() {
+    QRectF totalSizeRect {QRectF(QPointF(0, 0), this->size())};
+    const QPointF center {totalSizeRect.center()};
+    const double availableWidth {(1 - marginRelSize) * totalSizeRect.width()};
+    const double availableHeight {(1 - marginRelSize) * totalSizeRect.height()};
+    availableRect = {QRectF {center.x() - availableWidth / 2,
+                             center.y() - availableHeight / 2,
+                             availableWidth,
+                             availableHeight}};
+    pointBoxSize = {pointBoxRelSize * availableRect.height()};
+    emit sizeComputed();
+}
+
 void Plot::constructPointBoxes() {
     const double labelOffset = labelOffsetRelSize * availableRect.height();
-    const double scaleX = availableRect.width() / (rangeX.getSpan() - 1);
-    const double scaleY = (availableRect.height() - labelOffset) / (rangeY.getSpan() - 1);
+    const double scaleX = rangeX.span() > 0 ? availableRect.width() / (rangeX.span()) : 1;
+    const double scaleY = rangeY.span() > 0 ? (availableRect.height() - labelOffset) / (rangeY.span()) : 1;
     for (int graphNum = 0; graphNum < graphs.size(); ++graphNum) {
         QPointF referencePoint = availableRect.bottomLeft();
         pointBoxes[graphNum].clear();
@@ -89,93 +125,35 @@ void Plot::constructPointBoxes() {
     }
 }
 
-void Plot::replot() {
-    repaint();
+void Plot::paintGraph(const int graphNum, QPainter& painter) const {
+    const auto& graph = graphs[graphNum];
+    const auto& boxes = pointBoxes[graphNum];
+    QPolygonF centerPoints;
+    std::transform(boxes.cbegin(),
+                   boxes.cend(),
+                   std::back_inserter(centerPoints),
+                   [](const auto& box) { return box.position; });
+
+    painter.setPen(graph.pen());
+    painter.drawPolyline(centerPoints);
+    if (graph.showPoints()) {
+        paintPoints(boxes, painter);
+    }
 }
 
-void Plot::paintEvent(QPaintEvent*) {
-    QPainter painter(this);
-    painter.setRenderHint(QPainter::Antialiasing);
-    if (!adaptiveSizeComputed)
-        computeAdaptiveSizes();
-    if (pointBoxes.empty()) {
-        painter.eraseRect(availableRect);
-        return;
-    }
-
+void Plot::paintPoints(const PointBoxContainer& boxes, QPainter& painter) const {
     // Only labelSkip-th labels will be shown, so that labels are not cluttered with large span.
-    int labelSkip {static_cast<int>(rangeX.getSpan()) / labelSkipInd};
+    int labelSkip {static_cast<int>(rangeX.span()) / labelSkipInd};
     if (labelSkip < 1) labelSkip = 1;
 
-    for (int graphNum = 0; graphNum < graphs.size(); ++graphNum) {
-        const auto& graph = graphs[graphNum];
-        const auto& boxes = pointBoxes[graphNum];
-        QPolygonF centerPoints;
-        std::transform(boxes.cbegin(),
-                       boxes.cend(),
-                       std::back_inserter(centerPoints),
-                       [](const auto& box) { return box.position; });
-
-        painter.setPen(graph.getPen());
-        painter.drawPolyline(centerPoints);
-        if (!graph.showPoints())
-            continue;
-        for (int i = 0; i < boxes.size(); ++i) {
-            const auto& point = boxes[i];
-            painter.setBrush(pointBoxBrush);
-            painter.translate(point.position);
-            painter.drawPath(point.path);
-            painter.translate(-point.position);
-            if (i % labelSkip == 0)
-                painter.drawText(point.position.x(), availableRect.bottomLeft().y(), point.label);
-        }
-    }
-}
-
-void Plot::setRangeX(double start, double end) {
-    rangeX.setRange(start, end);
-}
-
-void Plot::setRangeY(double start, double end) {
-    rangeY.setRange(start, end);
-}
-
-void Plot::computeAdaptiveSizes() {
-    QRectF totalSizeRect {QRectF(QPointF(0, 0), this->size())};
-    const QPointF center {totalSizeRect.center()};
-    const double availableWidth {(1 - marginRelSize) * totalSizeRect.width()};
-    const double availableHeight {(1 - marginRelSize) * totalSizeRect.height()};
-    availableRect = {QRectF {center.x() - availableWidth / 2,
-                             center.y() - availableHeight / 2,
-                             availableWidth,
-                             availableHeight}};
-    pointBoxSize = {pointBoxRelSize * availableRect.height()};
-    emit sizeComputed();
-}
-
-void Plot::reset() {
-    for (auto& points : pointBoxes) {
-        points.clear();
-    }
-    for (auto& graph : graphs) {
-        graph.clearData();
-    }
-}
-
-void Plot::onSizeComputed() {
-    constructPointBoxes();
-    adaptiveSizeComputed = true;
-}
-
-void Plot::mouseMoveEvent(QMouseEvent* event) {
-    const auto& toolTip = getPosTooltip(event->pos());
-    if (toolTip.size() != 0) {
-        const QPoint toolTipPos {event->globalPos() - QPoint{int(pointBoxSize),
-                                 toolTipOffset * int(pointBoxSize)}};
-        QToolTip::showText(toolTipPos, toolTip);
-    } else {
-        QToolTip::hideText();
-        event->ignore();
+    for (int i = 0; i < boxes.size(); ++i) {
+        const auto& point = boxes[i];
+        painter.setBrush(pointBoxBrush);
+        painter.translate(point.position);
+        painter.drawPath(point.path);
+        painter.translate(-point.position);
+        if (i % labelSkip == 0)
+            painter.drawText(point.position.x(), availableRect.bottomLeft().y(), point.label);
     }
 }
 
@@ -190,4 +168,52 @@ const QString Plot::getPosTooltip(const QPoint& pos) const {
         }
     }
     return {""};
+}
+
+
+Graph::Graph() {
+
+}
+
+void Graph::setData(GraphData& data) {
+    points = data;
+    std::sort(points.begin(), points.end(), [](const auto& point1, const auto& point2) {
+            return point1.x < point2.x;
+        });
+}
+
+void Graph::setPen(QPen& pen) {
+    mPen = pen;
+}
+
+const QPen Graph::pen() const {
+    return mPen;
+}
+
+const GraphPoint& Graph::operator[](std::size_t idx) const {
+    return points[idx];
+}
+
+void Graph::clearData() {
+    points.clear();
+}
+
+void Graph::setShowPoints(bool showPoints) {
+    mShowPoints = showPoints;
+}
+
+bool Graph::showPoints() const {
+    return mShowPoints;
+}
+
+Graph::const_iterator Graph::cbegin() const {
+    return points.cbegin();
+}
+
+Graph::const_iterator Graph::cend() const {
+    return points.cend();
+}
+
+size_t Graph::size() const {
+    return points.size();
 }
--- src/gui/plot.h
+++ src/gui/plot.h
@@ -8,64 +8,26 @@
 #include <QEvent>
 #include <QGraphicsScene>
 
-#include <QDebug>
 
-
-struct GraphPoint
-{
-    double x;
-    double y;
-    QString label;
-};
-
+struct GraphPoint;
 
 using GraphData = QVector<GraphPoint>;
 
-
-class Graph
-{
-public:
-    using const_iterator = GraphData::const_iterator;
-    Graph();
-    virtual ~Graph() = default;
-    void setData(GraphData& data);
-    void setPen(QPen& pen);
-    const QPen getPen() const;
-    const GraphPoint& operator[](std::size_t idx) const;
-    void clearData();
-    void setShowPoints(bool showPoints);
-    bool showPoints() const;
-    const_iterator cbegin() const {
-        return points.cbegin();
-    }
-    const_iterator cend() const {
-        return points.cend();
-    }
-    size_t size() const;
-
-private:
-    QPen pen;
-    GraphData points;
-    bool mShowPoints = false;
-
-};
+class Graph;
 
 
 struct AxisRange {
+    // Set minimum and maximum values on axis.
+    // If start > end, then start is asssigned to end, and end to start.
+    void setRange(double start, double end);
+
+    // Return axis span, that is distance between minimum and maximum value.
+    double span() const;
+
+private:
     double start;
     double end;
 
-    void setRange(double start, double end) {
-        this->start = std::min(start, end);
-        this->end = std::max(start, end);
-    }
-
-    double getSpan() const {
-        if (end - start <= 1) {
-            return 2;
-        }
-        return end - start;
-    }
 };
 
 
@@ -82,24 +44,50 @@ private:
     };
 
 public:
+
     using PointBoxContainer = QVector<PointBox>;
+
     explicit Plot(QWidget* parent = 0);
+
     virtual ~Plot() = default;
-    void reset();
+
+    // Add Graph to plot. Multiple graphs can be added.
     void addGraph(Graph& graph);
+
+    // Set data points to graph with given number.
     void setGraphData(int graphNum, GraphData& data);
+
+    // Clear data points from all graphs in plot.
+    // Empty graphs remain attached to Plot.
+    void reset();
+
+    // Repaint plot. This method is ment to be called when graph data is changed.
     void replot();
+
+    // Set visible axis range.
     void setRangeX(double start, double end);
+
+    // Set visible axis range.
     void setRangeY(double start, double end);
 
 protected:
+
+    // Override to paint graphs.
     void paintEvent(QPaintEvent*) override;
+
+    // Override to show tooltip with point data when hovering over the point.
     void mouseMoveEvent(QMouseEvent* event) override;
 
 signals:
+
+    // Signal that is emitted when size of plot graphic primitives is computed.
+    // Size of plot depends on parent widget.
     void sizeComputed();
 
 private slots:
+
+    // Compute data required to draw points.
+    // Fired when plot graphic primitives' sizes are computed.
     void onSizeComputed();
 
 private:
@@ -118,9 +106,77 @@ private:
     constexpr static double marginRelSize {0.07};
     constexpr static double pointBoxRelSize {0.025};
 
+    // Compute sizes of plot graphic primitives that depend on size of Widget.
     void computeAdaptiveSizes();
+
+    // Compute data required to draw points relative to plot size.
     void constructPointBoxes();
+
+    // Paint graph with given number.
+    void paintGraph(const int graphNum, QPainter& painter) const;
+
+    // Paint graph points and labels.
+    void paintPoints(const PointBoxContainer& boxes, QPainter& painter) const;
+
+    // Show tooltip with point data when hovering over a point on a plot.
     const QString getPosTooltip(const QPoint& pos) const;
+};
+
+
+struct GraphPoint
+{
+    double x;
+    double y;
+    QString label;
+};
+
+
+class Graph
+{
+public:
+
+    using const_iterator = GraphData::const_iterator;
+
+    Graph();
+
+    virtual ~Graph() = default;
+
+    // Set data for this Graph.
+    void setData(GraphData& data);
+
+    // Set pen for drawing graph lines.
+    void setPen(QPen& pen);
+
+    // Return pen that is currently used for drawing graph lines.
+    const QPen pen() const;
+
+    // Overload for subscript operator to get access to graph points data.
+    const GraphPoint& operator[](std::size_t idx) const;
+
+    // Clear graph points.
+    void clearData();
+
+    // Draw data points and labels on graph if showPoints is true.
+    // Draw graph only if showPoints is false.
+    void setShowPoints(bool showPoints);
+
+    // Return true if data points are supposed to be drawn and false otherwise.
+    bool showPoints() const;
+
+    // Return iterator to the beginning (first data point)
+    const_iterator cbegin() const;
+
+    // Return iterator to the end.
+    const_iterator cend() const;
+
+    // Return number of points in graph.
+    size_t size() const;
+
+private:
+    QPen mPen;
+    GraphData points;
+    bool mShowPoints = false;
+
 };
 
 
