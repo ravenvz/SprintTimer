aefba19217c1b348d7ee0103a9da0424c0c66055
Pavel <texort@gmail.com>
2016-02-26 17:40:12 +0300

    Improve build system
    * Add clang's static analizer options in debug build
    * Build script now allows to choose from gcc or clang build
    * Build script now allows to choose release or debug build type

--- build_and_run_tests.sh
+++ build_and_run_tests.sh
@@ -3,14 +3,20 @@
 OPTIND=1
 
 build_type="Debug"
+c_compiler="gcc"
+cxx_compiler="g++"
 
-while getopts "r" opt ; do
+while getopts "rc" opt ; do
     case "$opt" in
         r) build_type="Release"
+            ;;
+        c) c_compiler="clang"
+           cxx_compiler="clang++"
+            ;;
     esac
 done
 
-echo $build_type
+echo "$build_type build using $cxx_compiler"
 
-(cd build && cmake -DCMAKE_BUILD_TYPE=$build_type .. && scan-build make -j$(nproc))
+(cd build && CC=$c_compiler CXX=$cxx_compiler cmake -DCMAKE_BUILD_TYPE=$build_type .. && make -j$(nproc))
 (cd bin && ./test_pomodoro)
