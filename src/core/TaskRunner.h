689bf306f404fdc90d5ada3d3d886fb8040dcef3
Pavel <texort@gmail.com>
2016-04-11 13:25:46 +0300

    Add documentation to TaskRunner

--- src/core/TaskRunner.h
+++ src/core/TaskRunner.h
@@ -1,19 +1,29 @@
 #ifndef TASKRUNNER_H_9VSDY5UR
 #define TASKRUNNER_H_9VSDY5UR
 
+#include "TaskScheduler.h"
+#include "Timer.h"
+#include "config.h"
+#include "date/date.h"
+#include "timeinterval.h"
 #include <chrono>
 #include <memory>
-#include "config.h"
-#include "Timer.h"
-#include "TaskScheduler.h"
-#include "timeinterval.h"
-#include "date/date.h"
 
+/* Responsible for running stopwatch by managing timer.
+ *
+ * Stopwatch initial time depends on type */
 class TaskRunner {
 public:
+    /* Construct TaskRunner given callback function, update
+     * interval and reference to application settings.
+     *
+     * When task is running, callback function will be executed after
+     * each update interval until time for the task runs out or task
+     * is cancelled. */
     TaskRunner(std::function<void(long timeLeft)> tickCallback,
                long updateIntervalInMilliseconds,
                const IConfig& applicationSettings);
+
     ~TaskRunner() = default;
 
     TaskRunner(TaskRunner&&) = delete;
@@ -22,16 +32,33 @@ public:
     TaskRunner(const TaskRunner&) = delete;
     TaskRunner& operator=(const TaskRunner&) = delete;
 
+    /* Start task.
+     *
+     * tickCallback function will be called each tick until time for the task
+     * runs out or cancelTask() method is called. */
     void startTask();
 
+    /* Cancel currently running task and stop timer. */
     void cancelTask();
 
+    /* Return TimeInterval for last task that had been completed normally
+     * (had not been cancelled).
+     *
+     * Note that interval's finish time will be recorded only after this method
+     * is called. */
     TimeInterval finishTask();
 
+    /* Return task duration in minutes.
+     *
+     * Duration is obtained from application config. */
     int taskDuration();
 
+    /* Return true if current task type is TaskScheduler::ShortBrake or
+     * TaskScheduler::LongBreak and false otherwise. */
     bool isBreak() const;
 
+    /* Toggles the TaskScheduler mode when it stops interleaving tasks with
+     * breaks and tasks are scheduled one after other without breaks. */
     void toggleInTheZoneMode();
 
 private:
