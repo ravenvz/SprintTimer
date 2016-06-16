9695db8621f13b23ee6bb4fcf0ea58588df8c3da
Pavel <texort@gmail.com>
2016-06-16 19:56:22 +0300

    Add TodoItemView implementation file and rename file

---
+++ qt_gui/src/widgets/TodoItemView.cpp
@@ -0,0 +1,107 @@
+#include "TodoItemView.h"
+
+TodoItemView::TodoItemView(QWidget* parent)
+    : QListView(parent)
+{
+    todoitemViewDelegate = new TodoItemsViewDelegate(this);
+    setItemDelegate(todoitemViewDelegate);
+
+    connect(this,
+            &QListView::customContextMenuRequested,
+            this,
+            &TodoItemView::showTodoItemContextMenu);
+}
+
+void TodoItemView::dropEvent(QDropEvent* event)
+{
+    int rowMovedFrom = currentIndex().row();
+    int rowMovedTo = indexAt(event->pos()).row();
+    model()->moveRows(
+        QModelIndex(), rowMovedFrom, 1, QModelIndex(), rowMovedTo);
+}
+
+void TodoItemView::setModels(TodoItemModel* taskModel, TagModel* tagModel)
+{
+    setModel(taskModel);
+    this->tagModel = tagModel;
+}
+
+void TodoItemView::editTodoItem()
+{
+    if (tagModel == nullptr) {
+        // TODO log error
+        qDebug() << "Passing null tagModel to TodoItemView";
+        return;
+    }
+    QModelIndex index = currentIndex();
+    // TODO provide proper implementation of data() and setData() in
+    // TodoItemModel, so that dynamic casts like that could be avoided
+    TodoItem itemToEdit
+        = dynamic_cast<TodoItemModel*>(model())->itemAt(index.row());
+    AddTodoItemDialog dialog{tagModel};
+    dialog.setWindowTitle("Edit TodoItem");
+    dialog.fillItemData(itemToEdit);
+    if (dialog.exec()) {
+        TodoItem updatedItem = dialog.constructedTask();
+        updatedItem.setSpentPomodoros(itemToEdit.spentPomodoros());
+        updatedItem.setCompleted(itemToEdit.isCompleted());
+        dynamic_cast<TodoItemModel*>(model())->replaceItemAt(index.row(),
+                                                             updatedItem);
+    }
+}
+
+void TodoItemView::removeTask()
+{
+    QModelIndex index = currentIndex();
+    ConfirmationDialog dialog;
+    // TODO figure out a way to handle this situation more gracefully
+    QString description;
+    if (dynamic_cast<TodoItemModel*>(model())
+            ->itemAt(index.row())
+            .spentPomodoros()
+        > 0) {
+        description
+            = "WARNING! This todo item has pomodoros associated with it "
+              "and they will be removed permanently along with this item.";
+    }
+    else {
+        description = "This will delete todo item permanently!";
+    }
+    dialog.setActionDescription(description);
+    if (dialog.exec()) {
+        dynamic_cast<TodoItemModel*>(model())->remove(index);
+    }
+}
+
+void TodoItemView::launchTagEditor()
+{
+    if (!tagEditor) {
+        tagEditor = new TagEditorWidget{tagModel};
+        tagEditor->show();
+    }
+    else {
+        tagEditor->raise();
+        tagEditor->activateWindow();
+        tagEditor->showNormal();
+    }
+}
+
+void TodoItemView::showTodoItemContextMenu(const QPoint& pos)
+{
+    QPoint globalPos = mapToGlobal(pos);
+
+    QMenu todoItemsMenu;
+    // Note QMenu takes ownership of Action
+    todoItemsMenu.addAction("Edit");
+    todoItemsMenu.addAction("Delete");
+    todoItemsMenu.addAction("Tag editor");
+
+    QAction* selectedItem = todoItemsMenu.exec(globalPos);
+
+    if (selectedItem && selectedItem->text() == "Edit")
+        editTodoItem();
+    if (selectedItem && selectedItem->text() == "Delete")
+        removeTask();
+    if (selectedItem && selectedItem->text() == "Tag editor")
+        launchTagEditor();
+}
---
+++ qt_gui/src/widgets/TodoItemView.h
@@ -0,0 +1,44 @@
+#ifndef TODOITEMVIEW_H
+#define TODOITEMVIEW_H
+
+#include "dialogs/AddTaskDialog.h"
+#include "dialogs/confirmationdialog.h"
+#include "models/TagModel.h"
+#include "models/todoitemmodel.h"
+#include "widgets/tageditorwidget.h"
+#include "widgets/todoitemsviewdelegate.h"
+#include <QDebug>
+#include <QDropEvent>
+#include <QListView>
+#include <QMenu>
+#include <QPointer>
+
+
+class TodoItemView : public QListView {
+public:
+    explicit TodoItemView(QWidget* parent);
+
+    // Overriding to catch dropEvent and prevent model start removing rows
+    // attempting drag and drop by calling another method instead.
+    void dropEvent(QDropEvent* event) final;
+
+    void setModels(TodoItemModel* taskModel, TagModel* tagModel);
+
+private:
+    QPointer<TodoItemsViewDelegate> todoitemViewDelegate;
+    TagModel* tagModel;
+    QPointer<TagEditorWidget> tagEditor;
+
+    void editTodoItem();
+
+    void removeTask();
+
+    void launchTagEditor();
+
+public slots:
+
+    void showTodoItemContextMenu(const QPoint& pos);
+};
+
+
+#endif /* end of include guard: TODOITEMVIEW_H */
