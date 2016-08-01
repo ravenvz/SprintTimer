a9e944c1aaaaa1243dc5cc364ab4a31899b68ff8
Pavel <texort@gmail.com>
2016-08-01 16:28:47 +0300

    Change build script

--- build_and_run_tests.sh
+++ build_and_run_tests.sh
@@ -9,10 +9,11 @@ memory_sanitizer="OFF"
 address_sanitizer="OFF"
 ub_sanitizer="OFF"
 thread_sanitizer="OFF"
+make_tool="make"
 make_options=-j$(nproc)
 debug_make_options="-j1 VERBOSE=1"
 
-while getopts "drcamut" opt ; do
+while getopts "drcamutn" opt ; do
     case "$opt" in
         d) make_options=$debug_make_options
             ;;
@@ -35,21 +36,28 @@ while getopts "drcamut" opt ; do
           address_sanitizer="OFF"
           ub_sanitizer="OFF"
            ;;
+       n) make_tool="ninja"
     esac
 done
 
-echo $make_options
+cmake_options="-DCMAKE_BUILD_TYPE=$build_type \
+               -DADDRESS_SANITIZER=$address_sanitizer \
+               -DMEMORY_SANITIZER=$memory_sanitizer \
+               -DUB_SANITIZER=$ub_sanitizer \
+               -DTHREAD_SANITIZER=$thread_sanitizer \
+               -DBUILD_TESTS=ON"
 
-echo "$build_type build using $cxx_compiler"
+if [[ $make_tool == 'ninja' ]]
+then
+    make_options=''
+    cmake_options+=' -GNinja'
+fi
 
-(cd build && CC=$c_compiler CXX=$cxx_compiler cmake \
-    -DCMAKE_BUILD_TYPE=$build_type \
-    -DADDRESS_SANITIZER=$address_sanitizer \
-    -DMEMORY_SANITIZER=$memory_sanitizer \
-    -DUB_SANITIZER=$ub_sanitizer \
-    -DTHREAD_SANITIZER=$thread_sanitizer \
-    -DBUILD_TESTS=ON \
-    .. && make $make_options)
+echo "$build_type build using $cxx_compiler and $make_tool"
+
+(cd build && \
+    CC=$c_compiler CXX=$cxx_compiler cmake $cmake_options .. \
+    && $make_tool $make_options)
 
 (cd bin &&
     ./test_pomodoro &&
