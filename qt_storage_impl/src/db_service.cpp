416cfe789349ce272036d20a872eeb963fbe2a19
Pavel <texort@gmail.com>
2016-08-13 19:22:51 +0300

    Moved db-related constants to namespaces

--- qt_storage_impl/src/db_service.cpp
+++ qt_storage_impl/src/db_service.cpp
@@ -253,85 +253,207 @@ bool Worker::createDbConnection()
     return true;
 }
 
+namespace TaskTable {
+const QString name{"todo_item"};
+
+namespace Columns {
+    const QString id{"id"};
+    const QString name{"name"};
+    const QString estimatedPomodoros{"estimated_pomodoros"};
+    const QString spentPomodoros{"spent_pomodoros"};
+    const QString completed{"completed"};
+    const QString priority{"priority"};
+    const QString lastModified{"last_modified"};
+    const QString uuid{"uuid"};
+}
+}
+
+namespace PomodoroTable {
+const QString name{"pomodoro"};
+
+namespace Columns {
+    const QString id{"id"};
+    const QString taskUuid{"todo_uuid"};
+    const QString startTime{"start_time"};
+    const QString finishTime{"finish_time"};
+    const QString uuid{"uuid"};
+}
+}
+
+namespace TagTable {
+const QString name{"tag"};
+
+namespace Columns {
+    const QString id{"id"};
+    const QString name{"name"};
+}
+}
+
+namespace TaskTagTable {
+const QString name{"todotag"};
+
+namespace Columns {
+    const QString id{"id"};
+    const QString tagId{"tag_id"};
+    const QString taskId{"todo_id"};
+    const QString taskUuid{"todo_uuid"};
+}
+}
+
+namespace TaskTagView
+{
+    const QString name{"task_tag_view"};
+
+    namespace Aliases {
+        const QString tagName{"tagname"};
+    }
+} /* TaskTagView */
+
+namespace TaskTagInsertTrigger {
+    const QString name{"instead_task_tag_view_insert"};
+}
+
+namespace CleanOrphanedTagTrigger {
+    const QString name{"on_todo_tag_delete"};
+}
+
+namespace PomoView {
+    const QString name{"pomodoro_view"};
+
+    namespace Aliases {
+        const QString tags{"tags"};
+    }
+}
+
+namespace PomoViewDeleteTrigger {
+    const QString name{"delete_from_pomodoro_view"};
+}
+
+namespace PomoViewInsertTrigger {
+    const QString name{"instead_pomodoro_view_insert"};
+}
+
+namespace TasksView {
+    const QString name{"task_view"};
+}
+
+namespace TaskViewDeleteTrigger {
+    const QString name{"on_task_view_delete"};
+}
+
+namespace TaskViewUpdateTrigger {
+    const QString name{"on_task_view_update"};
+}
+
+namespace CalendarTable {
+    const QString name{"calendar"};
+
+    namespace Columns {
+        const QString id{"id"};
+        const QString dt{"dt"};
+    }
+}
 
 bool Worker::createSchema()
 {
     QSqlQuery query;
 
-    QString createTodoItemTable = "CREATE TABLE todo_item "
-                                  "(id INTEGER PRIMARY KEY AUTOINCREMENT, "
-                                  "name VARCHAR, "
-                                  "estimated_pomodoros INTEGER, "
-                                  "spent_pomodoros INTEGER, "
-                                  "completed BOOLEAN, "
-                                  "priority INTEGER, "
-                                  "last_modified DATETIME, "
-                                  "uuid TEXT UNIQUE NOT NULL)";
+    const QString createTodoItemTable{
+        "CREATE TABLE " + TaskTable::name + "("
+            + TaskTable::Columns::id + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+        + TaskTable::Columns::name + " TEXT, "
+        + TaskTable::Columns::estimatedPomodoros + " INTEGER, "
+        + TaskTable::Columns::spentPomodoros + " INTEGER, "
+        + TaskTable::Columns::completed + " BOOLEAN, "
+        + TaskTable::Columns::priority + " INTEGER, "
+        + TaskTable::Columns::lastModified + " DATETIME, "
+        + TaskTable::Columns::uuid + " TEXT UNIQUE NOT NULL"
+    };
 
-    QString createPomodoroTable
-        = "CREATE TABLE pomodoro "
-          "(id INTEGER PRIMARY KEY AUTOINCREMENT, "
-          "todo_uuid TEXT, "
-          "start_time DATETIME, "
-          "finish_time DATETIME, "
-          "uuid TEXT UNIQUE NOT NULL, "
-          "FOREIGN KEY (todo_uuid) REFERENCES todo_item(uuid) "
-          "ON DELETE CASCADE)";
+    const QString createPomodoroTable{
+        "CREATE TABLE " + PomodoroTable::name + "("
+        + PomodoroTable::Columns::id + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+        + PomodoroTable::Columns::taskUuid + " TEXT, "
+        + PomodoroTable::Columns::startTime + " DATETIME, "
+        + PomodoroTable::Columns::finishTime + " DATETIME, "
+        + PomodoroTable::Columns::uuid + " TEXT UNIQUE NOT NULL, "
+        + "FOREIGN KEY (" + PomodoroTable::Columns::taskUuid + ") "
+        + "REFERENCES " + TaskTable::name + "(" + TaskTable::Columns::uuid + ")"
+        + " ON DELETE CASCADE)"
+    };
 
-    QString createTagTable = "CREATE TABLE tag "
-                             "(id INTEGER PRIMARY KEY AUTOINCREMENT, "
-                             "name VARCHAR(15) UNIQUE NOT NULL)";
+    const QString createTagTable{
+        "CREATE TABLE " + TagTable::name
+        + "(" + TagTable::Columns::id + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+        + TagTable::Columns::name + " TEXT UNIQUE NOT NULL)"
+    };
 
-    QString createTodoTagTable
-        = "CREATE TABLE todotag "
-          "(id INTEGER PRIMARY KEY AUTOINCREMENT, "
-          "tag_id INTEGER NOT NULL, "
-          "todo_id INTEGER NOT NULL, "
-          "todo_uuid TEXT NOT NULL, "
-          "FOREIGN KEY(tag_id) REFERENCES "
-          "tag(id) ON DELETE CASCADE ON UPDATE CASCADE, "
-          "FOREIGN KEY(todo_id) REFERENCES "
-          "todo_item(id) ON DELETE CASCADE ON UPDATE CASCADE)";
+    const QString createTodoTagTable{
+        "CREATE TABLE " + TaskTagTable::name + "("
+        + TaskTagTable::Columns::id + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+        + TaskTagTable::Columns::tagId + " INTEGER NOT NULL, "
+        + TaskTagTable::Columns::taskId + " INTEGER NOT NULL, "
+        + TaskTagTable::Columns::taskUuid + " TEXT NOT NULL, "
+        + "FOREIGN KEY(" + TaskTagTable::Columns::tagId + ")"
+        + " REFERENCES " + TagTable::name + "(" + TagTable::Columns::id + ")"
+        + " ON DELETE CASCADE ON UPDATE CASCADE, "
+        + "FOREIGN KEY(" + TaskTagTable::Columns::taskId + ")"
+        + " REFERENCES " + TaskTable::name + "(" + TaskTable::Columns::id + ")"
+        + " ON DELETE CASCADE ON UPDATE CASCADE)"
+    };
 
     // This view is a workaround to let insertion of tags
     // when we don't know tag id of already existing tag
-    QString createTaskTagView
-        = "CREATE VIEW task_tag_view AS "
-          "SELECT todo_item.id, "
-          "todo_item.name, "
-          "estimated_pomodoros, "
-          "spent_pomodoros, "
-          "priority, "
-          "completed, "
-          "tag.name tagname, "
-          "last_modified, "
-          "uuid "
-          "FROM todo_item "
-          "JOIN todotag ON todo_item.id = todotag.todo_id "
-          "LEFT JOIN tag ON todotag.tag_id = tag.id "
-          "ORDER BY todo_item.priority;";
+    QString createTaskTagView{
+        "CREATE VIEW " + TaskTagView::name + " AS "
+        + "SELECT " + TaskTable::name + "." + TaskTable::Columns::id + ", "
+        + TaskTable::name + "." + TaskTable::Columns::name + ", "
+        + TaskTable::Columns::estimatedPomodoros + ", "
+        + TaskTable::Columns::spentPomodoros + ", "
+        + TaskTable::Columns::priority + ", "
+        + TaskTable::Columns::completed + ", "
+        + TagTable::name + "." + TagTable::Columns::name + " " + TaskTagView::Aliases::tagName + ", "
+        + TaskTable::Columns::lastModified + ", "
+        + TaskTable::Columns::uuid
+        + " FROM " + TaskTable::name
+        + " JOIN " + TaskTagTable::name + " ON "
+        + TaskTable::name + "." + TaskTable::Columns::id + " = " + TaskTagTable::name + "." + TaskTagTable::Columns::taskId
+        + " LEFT JOIN " + TagTable::name + " ON "
+        + TaskTagTable::name + "." + TaskTagTable::Columns::tagId + " = " + TagTable::name + "." + TagTable::Columns::id
+        + " ORDER BY " + TaskTable::name + "." + TaskTable::Columns::priority + ";"
+    };
 
-    QString createInsteadOfTaskTagInsertTrigger
-        = "CREATE TRIGGER instead_task_tag_view_insert "
-          "INSTEAD OF INSERT on task_tag_view "
-          "BEGIN insert into tag(name) "
-          "SELECT NEW.tagname "
-          "WHERE (SELECT COUNT(name) FROM tag WHERE name = NEW.tagname) = 0; "
-          "INSERT OR IGNORE INTO todotag(tag_id, todo_id, todo_uuid) "
-          "VALUES((SELECT id FROM tag WHERE name = NEW.tagname), "
-          "(SELECT id FROM todo_item WHERE uuid = NEW.uuid), NEW.uuid); "
-          "END;";
+    const QString createInsteadOfTaskTagInsertTrigger{
+        "CREATE TRIGGER " + TaskTagInsertTrigger::name
+        + " INSTEAD OF INSERT ON " + TaskTagView::name
+        + " BEGIN INSERT INTO " + TagTable::name + "(" + TagTable::Columns::name +")"
+        + " SELECT NEW." + TaskTagView::Aliases::tagName
+        + " WHERE (SELECT COUNT(" + TagTable::Columns::name + ")"
+        + " FROM " + TagTable::name
+        + " WHERE " + TagTable::Columns::name + " = NEW." + TaskTagView::Aliases::tagName +") = 0; "
+        + " INSERT OR IGNORE INTO "
+        + TaskTagTable::name + "(" + TaskTagTable::Columns::tagId + ", " + TaskTagTable::Columns::taskId + ", " + TaskTagTable::Columns::taskUuid + ")"
+        + " VALUES((SELECT " + TagTable::Columns::id +
+        + " FROM " + TagTable::name
+        + " WHERE " + TagTable::Columns::name + " = NEW." + TaskTagView::Aliases::tagName + "), "
+        + "(SELECT " + TaskTable::Columns::id
+        + " FROM " + TaskTable::name
+        + " WHERE " + TaskTable::Columns::uuid + " = NEW." + TaskTable::Columns::uuid + "), NEW." + TaskTable::Columns::uuid + "); "
+        + "END;"
+    };
 
     // Trigger to remove orphaned tags (tags, that are not bound to any todo
     // item)
-    QString createCleanOrphanedTagTrigger
-        = "CREATE TRIGGER on_todotag_delete "
-          "AFTER DELETE ON todotag "
-          "BEGIN "
-          "DELETE FROM tag WHERE id = old.tag_id AND "
-          "(SELECT count(*) FROM todotag "
-          "WHERE tag_id = old.tag_id) = 0;"
-          "END;";
+    const QString createCleanOrphanedTagTrigger{
+        "CREATE TRIGGER " + CleanOrphanedTagTrigger::name
+        + " AFTER DELETE ON " + TaskTagTable::name
+        + " BEGIN "
+        + "DELETE FROM " + TagTable::name
+        + " WHERE " + TagTable::Columns::id + " = OLD." + TaskTagTable::Columns::tagId + " AND "
+        + "(SELECT count(*) FROM " + TaskTagTable::name
+        + " WHERE " + TagTable::Columns::id + " = OLD." + TaskTagTable::Columns::tagId + ") = 0; "
+        + "END;"
+    };
 
     // Trigger to increment spent pomodoros in todo_item when new pomodoro
     // is
@@ -355,86 +477,120 @@ bool Worker::createSchema()
     //       "END;";
 
     // View for pomodoros
-    QString createPomodoroView
-        = "CREATE VIEW pomodoro_view AS "
-          "SELECT pomodoro.id, pomodoro.todo_uuid, todo_item.name, "
-          "group_concat(tag.name) tags, start_time, finish_time, "
-          "pomodoro.uuid "
-          "FROM pomodoro join todo_item ON pomodoro.todo_uuid = "
-          "todo_item.uuid "
-          "LEFT JOIN todotag ON todotag.todo_id = todo_item.id "
-          "LEFT JOIN tag ON tag.id = todotag.tag_id "
-          "GROUP BY pomodoro.id;";
+    const QString createPomodoroView{
+        "CREATE VIEW " + PomoView::name + " AS "
+        + "SELECT " + PomodoroTable::name + "." + PomodoroTable::Columns::id + ", "
+        +  PomodoroTable::name + "." + PomodoroTable::Columns::taskUuid + ", "
+        +  TaskTable::name + "." + TaskTable::Columns::name + ", "
+        + "GROUP_CONCAT( " + TagTable::name + "." + TagTable::Columns::name + ") " + PomoView::Aliases::tags + ", "
+        + PomodoroTable::Columns::startTime + ", "
+        + PomodoroTable::Columns::finishTime + ", "
+        + PomodoroTable::name + "." + PomodoroTable::Columns::uuid
+        + " FROM " + PomodoroTable::name
+        + " JOIN " + TaskTable::name + " ON "
+        + PomodoroTable::name + "." + PomodoroTable::Columns::taskUuid + " = " + TaskTable::name + "." + TaskTable::Columns::uuid
+        + " LEFT JOIN " + TaskTagTable::name + " ON " + TaskTagTable::name + "." + TaskTagTable::Columns::taskId + " = " + TaskTable::name + "." + TaskTable::Columns::id
+        + " LEFT JOIN " + TagTable::name
+        + " ON " + TagTable::name + "." + TagTable::Columns::id + " = " + TaskTagTable::name + "." + TaskTagTable::Columns::tagId
+        + " GROUP BY " + PomodoroTable::name + "." + PomodoroTable::Columns::id + ";"
+    };
 
     // Trigger to remove from pomodoro_view as view are read-only in Sqlite3
-    QString createPomodoroViewDeleteTrigger
-        = "CREATE TRIGGER delete_from_pomodoro_view "
-          "INSTEAD OF DELETE ON pomodoro_view "
-          "BEGIN "
-          "DELETE FROM pomodoro WHERE uuid = old.uuid; "
-          "END;";
+    const QString createPomodoroViewDeleteTrigger{
+        "CREATE TRIGGER " + PomoViewDeleteTrigger::name
+        + " INSTEAD OF DELETE ON " + PomoView::name
+        + " BEGIN "
+        + "DELETE FROM " + PomodoroTable::name
+        + " WHERE " + PomodoroTable::Columns::uuid + " = OLD." + PomodoroTable::Columns::uuid + "; "
+        + " END;"
+    };
 
-    QString createPomodoroViewInsertTrigger
-        = "CREATE TRIGGER instead_pomodoro_view_insert "
-          "INSTEAD OF INSERT "
-          "ON pomodoro_view "
-          "BEGIN "
-          "INSERT INTO pomodoro (todo_uuid, start_time, finish_time, uuid) "
-          "SELECT new.todo_uuid, new.start_time, new.finish_time, new.uuid;"
-          "END;";
+    const QString createPomodoroViewInsertTrigger{
+        "CREATE TRIGGER " + PomoViewInsertTrigger::name
+        + " INSTEAD OF INSERT ON " + PomoView::name
+        + " BEGIN "
+        + "INSERT INTO " + PomodoroTable::name + "("
+        + PomodoroTable::Columns::taskUuid + ", "
+        + PomodoroTable::Columns::startTime + ", "
+        + PomodoroTable::Columns::finishTime + ", "
+        + PomodoroTable::Columns::uuid + ") "
+        + "SELECT"
+        + " NEW." + PomodoroTable::Columns::taskUuid
+        + ", NEW." + PomodoroTable::Columns::startTime
+        + ", NEW." + PomodoroTable::Columns::finishTime
+        + ", NEW." + PomodoroTable::Columns::uuid + "; "
+        + "END;"
+    };
 
     // View for todo items
-    QString createTaskView
-        = "CREATE VIEW task_view AS "
-          "SELECT todo_item.id, "
-          "todo_item.name, "
-          "estimated_pomodoros, "
-          "spent_pomodoros, "
-          "priority, "
-          "completed, "
+    const QString createTaskView{
+        "CREATE VIEW " + TasksView::name + " AS "
+        + "SELECT " + TaskTable::name + "." + TaskTable::Columns::id + ", "
+        + TaskTable::name + "." + TaskTable::Columns::name + ", "
+        + TaskTable::Columns::estimatedPomodoros + ", "
+        + TaskTable::Columns::spentPomodoros + ", "
+        + TaskTable::Columns::priority + ", "
+        + TaskTable::Columns::completed + ", "
           "GROUP_CONCAT(tag.name) tags, "
-          "last_modified, "
-          "uuid "
-          "FROM todo_item "
-          "LEFT JOIN todotag ON todo_item.id = todotag.todo_id "
-          "LEFT JOIN tag ON todotag.tag_id = tag.id "
-          "GROUP BY todo_item.id "
-          "ORDER BY todo_item.priority;";
+        + TaskTable::Columns::lastModified + ", "
+        + TaskTable::Columns::uuid
+        + " FROM " + TaskTable::name
+        + " LEFT JOIN " + TaskTagTable::name + " ON "
+        + TaskTable::name + "." + TaskTable::Columns::id + " = " + TaskTagTable::name + "." + TaskTable::Columns::id
+        + " LEFT JOIN " + TagTable::name +  " ON "
+        + TaskTagTable::name + "." + TaskTagTable::Columns::tagId + " = " + TagTable::name + "." + TagTable::Columns::id
+        + " GROUP BY " + TaskTable::name + "." + TaskTable::Columns::id
+        + " ORDER BY " + TaskTable::name + "." + TaskTable::Columns::priority + ";"
+    };
 
     // Trigger to remove from task_view
-    QString createTaskViewDeleteTrigger
-        = "CREATE TRIGGER on_task_view_delete "
-          "INSTEAD OF DELETE ON task_view "
-          "BEGIN "
-          "DELETE FROM todo_item WHERE id = old.id; "
-          "END;";
+    const QString createTaskViewDeleteTrigger{
+        "CREATE TRIGGER " + TaskViewDeleteTrigger::name
+        + " INSTEAD OF DELETE ON " + TasksView::name
+        + " BEGIN "
+        + "DELETE FROM " + TaskTable::name
+        + " WHERE " + TaskTable::Columns::id + " = OLD." + TaskTable::Columns::id + ";"
+        + " END;"
+    };
 
     // Trigger on update on task_view
-    QString createTaskViewUpdateTrigger
-        = "CREATE TRIGGER on_task_view_update "
-          "INSTEAD OF UPDATE ON task_view "
-          "BEGIN "
-          "UPDATE todo_item SET name = new.name, "
-          "estimated_pomodoros = new.estimated_pomodoros, "
-          "spent_pomodoros = new.spent_pomodoros, "
-          "priority = new.priority, "
-          "completed = new.completed, "
-          "last_modified = new.last_modified "
-          "WHERE id = old.id; "
-          "END;";
+    const QString createTaskViewUpdateTrigger{
+        "CREATE TRIGGER " + TaskViewUpdateTrigger::name
+        + " INSTEAD OF UPDATE ON " + TasksView::name
+        + " BEGIN "
+        + "UPDATE " + TaskTable::name
+        + " SET " + TaskTable::Columns::name + " = NEW." + TaskTable::Columns::name + ", "
+        + TaskTable::Columns::estimatedPomodoros + " = NEW." + TaskTable::Columns::estimatedPomodoros + ", "
+        + TaskTable::Columns::spentPomodoros + " = NEW." + TaskTable::Columns::spentPomodoros + ", "
+        + TaskTable::Columns::priority + " = NEW." + TaskTable::Columns::priority + ", "
+        + TaskTable::Columns::completed + " = NEW." + TaskTable::Columns::completed + ", "
+        + TaskTable::Columns::lastModified + " = NEW." + TaskTable::Columns::lastModified
+        + " WHERE " + TaskTable::Columns::id + " = OLD." + TaskTable::Columns::id + ";"
+        + " END;"
+    };
 
-    // Create calendar table
-    query.exec("CREATE TABLE calendar (id INTEGER PRIMARY KEY)");
-    query.exec("INSERT INTO calendar DEFAULT VALUES");
-    query.exec("INSERT INTO calendar DEFAULT VALUES");
-    query.exec("INSERT INTO calendar SELECT NULL FROM calendar d1, calendar "
-               "d2, calendar d3, calendar d4");
-    query.exec("INSERT INTO calendar SELECT NULL FROM calendar d1, calendar "
-               "d2, calendar d3, calendar d4");
-    query.exec("ALTER TABLE calendar ADD dt DATETIME");
+    // Create calendar table as temporary solution for missing dates problem
+    query.exec("CREATE TABLE " + CalendarTable::name
+            + " (" + CalendarTable::Columns::id + " INTEGER PRIMARY KEY)");
+    query.exec("INSERT INTO " + CalendarTable::name + " DEFAULT VALUES");
+    query.exec("INSERT INTO " + CalendarTable::name + " DEFAULT VALUES");
+    query.exec("INSERT INTO " + CalendarTable::name + " SELECT NULL FROM "
+            + CalendarTable::name + "d1, "
+            + CalendarTable::name + "d2, "
+            + CalendarTable::name + "d3, "
+            + CalendarTable::name + "d4"
+            );
+    query.exec("INSERT INTO " + CalendarTable::name + " SELECT NULL FROM "
+            + CalendarTable::name + " d1, "
+            + CalendarTable::name + " d2, "
+            + CalendarTable::name + " d3, "
+            + CalendarTable::name + " d4 "
+            );
+    query.exec("ALTER TABLE " + CalendarTable::name + " ADD " + CalendarTable::Columns::dt + " DATETIME");
     query.exec(
-        "UPDATE calendar SET dt = date('2000-01-01', (-1 + id) ||' day')");
-    query.exec("CREATE INDEX idx1 ON calendar(dt)");
+        "UPDATE " + CalendarTable::name + " SET " + CalendarTable::Columns::dt
+        + " = date('2000-01-01', (-1 + id) ||' day')");
+    query.exec("CREATE INDEX idx1 ON " + CalendarTable::name + "(" + CalendarTable::Columns::dt + ")");
 
     return execAndCheck(query, createTodoItemTable)
         && execAndCheck(query, createTagTable)
