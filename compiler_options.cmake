2939af33b20be9b595d04de4cd621c00e567634b
Pavel <texort@gmail.com>
2016-09-01 15:56:03 +0300

    More strict compiler options for clang

--- compiler_options.cmake
+++ compiler_options.cmake
@@ -7,13 +7,11 @@ set(CLANG_COMPILE_OPTIONS -ftemplate-backtrace-limit=0
                           -Wno-weak-vtables
                           -Wno-padded
                           -Wno-covered-switch-default
-                          -Wno-shadow
+                          -Wno-shadow-field-in-constructor
                           -Wno-missing-prototypes
                           -Wno-unused-parameter
-                          -Wno-global-constructors
-                          -Wno-missing-noreturn
                           -Wno-undefined-reinterpret-cast # Qt's mock
-                          -Wno-exit-time-destructors)
+                          )
 
 set(GCC_COMPILE_OPTIONS -ftemplate-backtrace-limit=0
                         -Wall
