a78e90e32a8b41a3a41219b044212100304cfca7
Pavel <texort@gmail.com>
2018-05-28 21:21:43 +0300

    Long task name is now displayed correctly

--- qt_gui/src/widgets/TaskViewDelegate.cpp
+++ qt_gui/src/widgets/TaskViewDelegate.cpp
@@ -19,47 +19,60 @@
 ** along with SprintTimer.  If not, see <http://www.gnu.org/licenses/>.
 **
 *********************************************************************************/
-#include <QPainter>
-#include <QApplication>
 #include "TaskViewDelegate.h"
+#include <QApplication>
+#include <QPainter>
 
+namespace {
 
-QRect constructBoundingRect(const QStyleOptionViewItem& option, const QString& text, int flags);
+QRect constructBoundingRect(const QStyleOptionViewItem& option,
+                            const QString& text,
+                            int flags);
 
-void rescaleTagsRectIfMultiline(QRect& tagsRect, const QRect& statRect, const QRect& optionRect);
+void rescaleTagsRectIfMultiline(QRect& tagsRect,
+                                const QRect& statRect,
+                                const QRect& optionRect);
 
 bool taskOverspent(const QString& stats);
 
-TaskViewDelegate::TaskViewDelegate(QObject* parent) :
-    QStyledItemDelegate(parent)
+} // namespace
+
+
+TaskViewDelegate::TaskViewDelegate(QObject* parent)
+    : QStyledItemDelegate(parent)
 {
     delimiterColor.setAlpha(50);
 }
 
-QSize TaskViewDelegate::sizeHint(const QStyleOptionViewItem& option, const QModelIndex& index) const {
-    if (!index.isValid()) return QSize();
+QSize TaskViewDelegate::sizeHint(const QStyleOptionViewItem& option,
+                                 const QModelIndex& index) const
+{
+    if (!index.isValid())
+        return QSize();
 
     QString tags = index.data(Qt::UserRole + 1).toString();
     QString name = index.data(Qt::UserRole + 2).toString();
     QString completionStats = index.data(Qt::UserRole + 3).toString();
 
-    QRect tagsRect {constructBoundingRect(option, tags, Qt::AlignLeft)};
-    QRect statRect {constructBoundingRect(option, completionStats, Qt::AlignRight)};
-    QRect nameRect {constructBoundingRect(option, name, Qt::AlignLeft | Qt::TextWordWrap)};
+    QRect tagsRect{constructBoundingRect(option, tags, Qt::AlignLeft)};
+    QRect statRect{
+        constructBoundingRect(option, completionStats, Qt::AlignRight)};
+    QRect nameRect{
+        constructBoundingRect(option, name, Qt::AlignLeft | Qt::TextWordWrap)};
 
     rescaleTagsRectIfMultiline(tagsRect, statRect, option.rect);
 
-    return QSize {option.rect.width(), tagsRect.height() + nameRect.height() + 3 * padding + verticalSpacing};
+    return QSize{option.rect.width(),
+                 tagsRect.height() + nameRect.height() * padding
+                     + verticalSpacing};
 }
 
-QRect constructBoundingRect(const QStyleOptionViewItem& option, const QString& text, int flags) {
-    QFontMetrics metrics {option.font};
-    return QRect {metrics.boundingRect(0, 0, option.rect.width(), 0, flags, text)};
-}
-
-void TaskViewDelegate::paint(QPainter *painter, const QStyleOptionViewItem &option, const QModelIndex &index) const {
+void TaskViewDelegate::paint(QPainter* painter,
+                             const QStyleOptionViewItem& option,
+                             const QModelIndex& index) const
+{
     painter->save();
-    QFont completedItemFont {option.font};
+    QFont completedItemFont{option.font};
     painter->setFont(completedItemFont);
     if (option.state & QStyle::State_Selected) {
         painter->fillRect(option.rect, option.palette.highlight());
@@ -68,63 +81,103 @@ void TaskViewDelegate::paint(QPainter *painter, const QStyleOptionViewItem &opti
     if (index.data(Qt::CheckStateRole).toBool()) {
         completedItemFont.setStrikeOut(true);
         painter->setFont(completedItemFont);
-        paintText(painter, option, index, completedItemAlpha);
-    } else {
-        paintText(painter, option, index, normalItemAlpha);
+        paintItem(painter, option, index, completedItemAlpha);
+    }
+    else {
+        paintItem(painter, option, index, normalItemAlpha);
     }
     painter->restore();
 }
 
-void TaskViewDelegate::paintText(QPainter* painter, const QStyleOptionViewItem& option, const QModelIndex& index, int alpha) const {
-    QColor tagCol = tagColor;
-    QColor commonCol = commonColor;
-    QColor overspentCol = overspentColor;
+void TaskViewDelegate::paintItem(QPainter* painter,
+                                 const QStyleOptionViewItem& option,
+                                 const QModelIndex& index,
+                                 int alpha) const
+{
+    QColor tagCol{tagColor};
+    QColor commonCol{commonColor};
+    QColor overspentCol{overspentColor};
+
     tagCol.setAlpha(alpha);
     commonCol.setAlpha(alpha);
     overspentCol.setAlpha(alpha);
 
-    QString tags = index.data(Qt::UserRole + 1).toString();
-    QString descr = index.data(Qt::UserRole + 2).toString();
-    QString completionStats = index.data(Qt::UserRole + 3).toString();
+    const QString tags = index.data(Qt::UserRole + 1).toString();
+    const QString descr = index.data(Qt::UserRole + 2).toString();
+    const QString completionStats = index.data(Qt::UserRole + 3).toString();
 
-    // Stats should be drawn in the bottom-right corner of the line where tags are drawn
-    // So we set tagsRect width to match item width and add corrections in height.
-    QRect tagsRect {constructBoundingRect(option, tags, Qt::AlignLeft | Qt::TextWordWrap)};
+    // Stats should be drawn in the top-right corner of the line where tags
+    // are drawn So we set tagsRect width to match item width and add
+    // corrections in height.
+    QRect tagsRect{
+        constructBoundingRect(option, tags, Qt::AlignLeft | Qt::TextWordWrap)};
     tagsRect.setWidth(option.rect.width());
-    tagsRect.adjust(option.rect.topLeft().x(), option.rect.topLeft().y() + verticalSpacing,
-                    0, option.rect.topLeft().y() + verticalSpacing);
+    tagsRect.adjust(option.rect.topLeft().x(),
+                    option.rect.topLeft().y() + verticalSpacing,
+                    0,
+                    option.rect.topLeft().y() + verticalSpacing);
 
     painter->setPen(tagCol);
-    painter->drawText(tagsRect, Qt::AlignLeft | Qt::AlignTop | Qt::TextWordWrap, tags);
+    painter->drawText(
+        tagsRect, Qt::AlignLeft | Qt::AlignTop | Qt::TextWordWrap, tags);
+
+    QRect descrRect{
+        constructBoundingRect(option, descr, Qt::AlignLeft | Qt::TextWordWrap)};
+    descrRect.adjust(option.rect.bottomLeft().x(),
+                     option.rect.bottomLeft().y() - verticalSpacing,
+                     0,
+                     option.rect.bottomLeft().y() - verticalSpacing);
 
     painter->setPen(commonCol);
-    painter->drawText(option.rect, Qt::AlignLeft | Qt::TextWordWrap | Qt::AlignBottom, descr);
+    painter->drawText(
+        descrRect, Qt::AlignLeft | Qt::TextWordWrap | Qt::AlignBottom, descr);
 
     if (taskOverspent(completionStats)) {
         painter->setPen(overspentCol);
     }
-    painter->drawText(tagsRect, Qt::AlignRight | Qt::AlignBottom, completionStats);
+    painter->drawText(
+        tagsRect, Qt::AlignRight | Qt::AlignBottom, completionStats);
 
-    QPoint lineBegin {tagsRect.topLeft().x() + 2 * padding,
-                      tagsRect.topLeft().y() - verticalSpacing / 2};
-    QPoint lineEnd {tagsRect.topLeft().x() + tagsRect.width() - 1 * padding,
-                    tagsRect.topLeft().y() - verticalSpacing / 2};
+    QPoint lineBegin{tagsRect.topLeft().x() + 2 * padding,
+                     tagsRect.topLeft().y() - verticalSpacing / 2};
+    QPoint lineEnd{tagsRect.topLeft().x() + tagsRect.width() - 1 * padding,
+                   tagsRect.topLeft().y() - verticalSpacing / 2};
     if (index.row() != 0) {
         painter->setPen(delimiterColor);
         painter->drawLine(lineBegin, lineEnd);
     }
 }
 
-void rescaleTagsRectIfMultiline(QRect& tagsRect, const QRect& statRect, const QRect& optionRect) {
-    if (optionRect.width() > 0 && (tagsRect.width() + statRect.width()) > optionRect.width()) {
 
-        int scaleFactor = (tagsRect.width() + statRect.width()) / optionRect.width() + 1;
-        tagsRect.setHeight(scaleFactor * tagsRect.height() + scaleFactor * TaskViewDelegate::padding);
+namespace {
+
+QRect constructBoundingRect(const QStyleOptionViewItem& option,
+                            const QString& text,
+                            int flags)
+{
+    QFontMetrics metrics{option.font};
+    return QRect{
+        metrics.boundingRect(0, 0, option.rect.width(), 0, flags, text)};
+}
+
+void rescaleTagsRectIfMultiline(QRect& tagsRect,
+                                const QRect& statRect,
+                                const QRect& optionRect)
+{
+    if (optionRect.width() > 0
+        && (tagsRect.width() + statRect.width()) > optionRect.width()) {
+
+        int scaleFactor
+            = (tagsRect.width() + statRect.width()) / optionRect.width() + 1;
+        tagsRect.setHeight(scaleFactor * tagsRect.height()
+                           + scaleFactor * TaskViewDelegate::padding);
     }
 }
 
-bool taskOverspent(const QString& stats) {
+bool taskOverspent(const QString& stats)
+{
     QStringList parts = stats.split("/");
-    return (parts[0].toInt() > parts[1].toInt());
+    return parts[0].toInt() > parts[1].toInt();
 }
 
+} // namespace
--- qt_gui/src/widgets/TaskViewDelegate.h
+++ qt_gui/src/widgets/TaskViewDelegate.h
@@ -26,30 +26,33 @@
 #include <QStyledItemDelegate>
 
 
-class TaskViewDelegate : public QStyledItemDelegate
-{
+class TaskViewDelegate : public QStyledItemDelegate {
     Q_OBJECT
 
 public:
     explicit TaskViewDelegate(QObject* parent);
 
-    void paint(QPainter* painter, const QStyleOptionViewItem& option, const QModelIndex& index) const override;
-    QSize sizeHint(const QStyleOptionViewItem& option, const QModelIndex& index) const override;
+    void paint(QPainter* painter,
+               const QStyleOptionViewItem& option,
+               const QModelIndex& index) const override;
+    QSize sizeHint(const QStyleOptionViewItem& option,
+                   const QModelIndex& index) const override;
 
-    static constexpr int padding {1};
-    static constexpr int verticalSpacing {14};
+    static constexpr int padding{1};
+    static constexpr int verticalSpacing{14};
 
 private:
+    void paintItem(QPainter* painter,
+                   const QStyleOptionViewItem& option,
+                   const QModelIndex& index,
+                   int alpha) const;
 
-    void paintText(QPainter* painter, const QStyleOptionViewItem& option, const QModelIndex& index, int alpha) const;
-
-    QColor tagColor {Qt::blue};
-    QColor commonColor {Qt::black};
-    QColor overspentColor {Qt::red};
-    QColor delimiterColor {Qt::gray};
-    const int completedItemAlpha {100};
-    const int normalItemAlpha {255};
-
+    QColor tagColor{Qt::blue};
+    QColor commonColor{Qt::black};
+    QColor overspentColor{Qt::red};
+    QColor delimiterColor{Qt::gray};
+    const int completedItemAlpha{100};
+    const int normalItemAlpha{255};
 };
 
 #endif /* end of include guard: TASKVIEWDELEGATE_H_Z8NDXQM6 */
