e0d9d9683f549337bff84ecd4730ead23625d7a6
Pavel <texort@gmail.com>
2018-06-23 11:02:10 +0300

    Remove old Command interface

--- core/include/core/Command.h
+++ core/include/core/Command.h
@@ -39,18 +39,4 @@ public:
 
 } // namespace sprint_timer
 
-class Command {
-public:
-    virtual ~Command() = default;
-
-    virtual void execute() = 0;
-
-    virtual void undo() {}
-
-    virtual bool supportUndo() const { return false; }
-
-    virtual std::string inspect() const = 0;
-};
-
-
 #endif /* end of include guard: ICOMMAND_H_NHSUJX60 */
