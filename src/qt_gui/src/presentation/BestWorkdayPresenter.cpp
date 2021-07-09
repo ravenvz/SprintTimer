a289420fba606ccb7bb6af8c3507496a0336d427
Pavel <texort@gmail.com>
2021-07-09 12:23:35 +0300

    Fix span initialization

--- src/qt_gui/src/presentation/BestWorkdayPresenter.cpp
+++ src/qt_gui/src/presentation/BestWorkdayPresenter.cpp
@@ -108,7 +108,7 @@ void BestWorkdayPresenter::updateBars(
                     end(values));
         v.value()->displayBars(View::BarD{std::string{barBorderColor},
                                           std::string{barColor},
-                                          std::span<double, 7>{values.begin(), values.end()},
+                                          std::span<const double, 7>{values.cbegin(), values.cend()},
                                           firstDayOfWeek == dw::Weekday::Monday
                                               ? mondayFirstOrder
                                               : sundayFirstOrder});
